7f3321fa719fd7a612c4a2418e86420529deaef6
Arimah <arimah42@gmail.com>
2020-09-06 01:38:51 +0200

    Make even more minimal

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 function foo<T>(x: TemplateStringsArray, ...y: T[]) {
   // (irrelevant code to get rid of unused variable warnings)
-  return [x[0],  y[0]];
+  return [x,  y];
 }
 
-interface FalsePositive { something: unknown; }
+interface FalsePositive { }
 //        ^^^^^^^^^^^^^ @typescript-eslint/no-unused-vars - false positive
 
 foo<FalsePositive>``;
